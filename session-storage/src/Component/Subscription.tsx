2f3ca0c2c77c18a282a3cf3894bd98e559811eaf
Ho Nyeong <dev.honing@gmail.com>
2024-01-10 21:42:27 +0900

    Feat: Subscription 함수 정의
    - 기본 틀을 생성하고, Subscription으로 export

--- session-storage/src/Component/Subscription.tsx
+++ session-storage/src/Component/Subscription.tsx
@@ -1,2 +1,12 @@
 // src/Component/Subscription.tsx
 
+import React from 'react';
+
+const Subscription: React.FC = () => {
+
+  return (<div>
+    Hello, sessionStorage!
+  </div>);
+}
+
+export default Subscription;
